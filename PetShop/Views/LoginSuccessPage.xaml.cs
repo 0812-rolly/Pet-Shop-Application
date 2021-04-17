d625c7dc9b8ef516ba47458cb915b06279c0b184
ilyataster <taster00@gmail.com>
2021-04-17 21:39:19 +0400

    success login page

---
+++ PetShop/Views/LoginSuccessPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PetShop.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginSuccessPage : ContentPage
+    {
+        public LoginSuccessPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
