34867eba50dcb572ad54e11b02c97d90934e5839
ilyataster <taster00@gmail.com>
2021-04-17 23:02:59 +0400

    resolving conflicts

---
+++ PetShop/Views/HelloPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PetShop.Views
+{
+    public partial class HelloPage : ContentPage
+    {
+        public HelloPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
