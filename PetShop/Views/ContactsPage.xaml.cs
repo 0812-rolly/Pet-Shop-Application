819d8e6c8370dbb0bcde518369d178736f1664ec
Macr0pyre <iraengels2000@gmail.com>
2021-04-18 15:53:17 +0400

    Without NavigationBar on ContactsPage and LoginSuccessPage

--- PetShop/Views/ContactsPage.xaml.cs
+++ PetShop/Views/ContactsPage.xaml.cs
@@ -14,6 +14,7 @@ namespace PetShop.Views
     {
         public ContactsPage()
         {
+            NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
         }
     }
