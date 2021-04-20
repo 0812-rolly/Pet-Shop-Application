5d4a89fe46afa426e3ca66db66ec229c4d8b5c8a
ilyataster <taster00@gmail.com>
2021-04-20 15:00:44 +0400

    login pages update

---
+++ PetShop/Views/LoginConfirmPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PetShop.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginConfirmPage : ContentPage
+    {
+        public LoginConfirmPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            bool error = false;
+
+            if (etr_SmsCode.Text == null || etr_SmsCode.Text == "")
+            {
+                etr_SmsCode.PlaceholderColor = Color.FromHex("#f56b62");
+                error = true;
+            }
+
+            if (error == false)
+                await Navigation.PushAsync(new LoginSuccessPage());
+        }
+    }
+}
--- PetShop/Views/LoginPage.xaml.cs
+++ PetShop/Views/LoginPage.xaml.cs
@@ -16,6 +16,26 @@ namespace PetShop.Views
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            btn_Login.Clicked += (sender, args) => OnButtonClicked(sender, args);
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e) 
+        {
+            bool error = false;
+
+            if (etr_PhoneNumber.Text == null || etr_PhoneNumber.Text == "")
+            {
+                etr_PhoneNumber.PlaceholderColor = Color.FromHex("#f56b62");
+                error = true;
+            }
+
+            if (Cb_Agreement.IsChecked == false)
+            {
+                error = true;
+            }
+
+            if (error == false)
+                await Navigation.PushAsync(new LoginConfirmPage());
         }
     }
 }
